558784ec12e9f83bd903b736bff825be232e25ba
Kwonkyu <park2348190@naver.com>
2021-06-20 16:08:04 +0900

    Add missing request header exception handler

--- src/main/java/com/simpletodolist/todolist/exception/GeneralExceptionHandler.java
+++ src/main/java/com/simpletodolist/todolist/exception/GeneralExceptionHandler.java
@@ -3,6 +3,7 @@ package com.simpletodolist.todolist.exception;
 import com.simpletodolist.todolist.exception.general.AuthenticationFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,5 +32,12 @@ public class GeneralExceptionHandler {
     @ResponseBody
     public ExceptionResponseDTO authenticationFailed(MethodArgumentNotValidException exception) {
         return new ExceptionResponseDTO("Parameter Value Constraint Not Matched.", "Check API requirements.");
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ExceptionResponseDTO missingRequestHeader(MissingRequestHeaderException exception) {
+        return new ExceptionResponseDTO("Requested Header Not Present", "Check API requirements");
     }
 }
