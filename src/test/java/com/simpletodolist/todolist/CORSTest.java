75a8a5ca47e5d2c8884170c39e831ff0d17f181e
Kwonkyu <park2348190@naver.com>
2021-11-09 20:48:06 +0900

    Updated CORS test.

--- src/test/java/com/simpletodolist/todolist/CORSTest.java
+++ src/test/java/com/simpletodolist/todolist/CORSTest.java
@@ -2,9 +2,8 @@ package com.simpletodolist.todolist;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.simpletodolist.todolist.domain.bind.MemberDTO;
+import com.simpletodolist.todolist.controller.bind.member.MemberInformationRequest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -21,20 +21,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class CORSTest {
-
+@Transactional
+class CORSTest {
     @Autowired
     MockMvc mockMvc;
     @Autowired
     ObjectMapper objectMapper;
 
     @Test
-    public void corsRequest() throws Exception {
+    void corsRequest() throws Exception {
+        MemberInformationRequest request = new MemberInformationRequest();
+        request.setUsername("CORS_USERNAME");
+        request.setAlias("CORS_ALIAS");
+        request.setPassword("CORS_PASSWORD");
+
         mockMvc.perform(post("/api/public/register")
                 .header(HttpHeaders.ORIGIN, "www.intellij.com")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(MemberDTO.RegisterRequest.builder().userId("corsuserid").username("corsusername").password("corspassword").build())))
+                .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
                 .andExpect(header().exists(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN))
                 .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "www.intellij.com"));
