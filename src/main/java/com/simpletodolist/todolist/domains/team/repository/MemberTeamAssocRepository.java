4db1b105b097ec08304cd386ddbbec735ff0c384
Kwonkyu <park2348190@naver.com>
2022-01-30 15:01:00 +0900

    removed team old codes

--- src/main/java/com/simpletodolist/todolist/domains/team/repository/MemberTeamAssocRepository.java
+++
@@ -1,7 +0,0 @@
-package com.simpletodolist.todolist.domains.team.repository;
-
-import com.simpletodolist.todolist.domains.team.entity.MemberTeamAssociation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberTeamAssocRepository extends JpaRepository<MemberTeamAssociation, Long> {
-}
