cf71423ebe754738bcd7cb3988d407e09211e3f2
Kwonkyu <park2348190@naver.com>
2022-01-30 15:01:49 +0900

    removed old jwt features

--- src/main/java/com/simpletodolist/todolist/domains/jwt/repository/UserJwtRepository.java
+++
@@ -1,7 +0,0 @@
-package com.simpletodolist.todolist.domains.jwt.repository;
-
-import com.simpletodolist.todolist.domains.jwt.UserJwt;
-import org.springframework.data.keyvalue.repository.KeyValueRepository;
-
-public interface UserJwtRepository extends KeyValueRepository<UserJwt, String> {
-}
